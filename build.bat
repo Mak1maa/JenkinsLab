eec6d61a5d7be24a19e464c01efc8092f4174fc0
Makimaa <63280722+Mak1maa@users.noreply.github.com>
2021-11-09 14:57:07 +0400

    jenkins

---
+++ build.bat
@@ -0,0 +1,6 @@
+MD C:\Results\bin\Jenkins
+MD C:\Results\bin\JenkinsTest
+cd C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\
+dotnet restore C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob
+msbuild.exe C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsTest /t:Build /p:OutputPath="C:\Results\bin\Jenkins"
+msbuild.exe C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsTestUnitTest /t:Build /p:OutputPath="C:\Results\bin\JenkinsTest"
