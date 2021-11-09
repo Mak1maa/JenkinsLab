011383b3828f7e8c47ddd2adcc191937c8f9582d
Makimaa <63280722+Mak1maa@users.noreply.github.com>
2021-11-09 15:28:17 +0400

    new message

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 MD C:\Results\bin\Jenkins
 MD C:\Results\bin\JenkinsTest
 cd C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\
-dotnet restore C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsLab
-msbuild.exe C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsLab\Jenkins /t:Build /p:OutputPath="C:\Results\bin\Jenkins"
-msbuild.exe C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsLab\JenkinsMsTest /t:Build /p:OutputPath="C:\Results\bin\JenkinsTest"
+dotnet restore C:\Windows\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsLab
+msbuild.exe C:\Windows\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsLab\Jenkins /t:Build /p:OutputPath="C:\Results\bin\Jenkins"
+msbuild.exe C:\Windows\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsLab\JenkinsMsTest /t:Build /p:OutputPath="C:\Results\bin\JenkinsTest"
