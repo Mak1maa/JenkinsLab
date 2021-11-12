ece78a6a0a5ea7c34b8bd529aaea9255c65f4d4c
Makimaa <63280722+Mak1maa@users.noreply.github.com>
2021-11-12 11:58:01 +0400

    de

--- JenkinsMsTest/obj/Release/netcoreapp2.1/JenkinsMsTest.AssemblyInfo.cs
+++ JenkinsMsTest/obj/Release/netcoreapp2.1/JenkinsMsTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Jenkins")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("JenkinsMsTest")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Jenkins")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Jenkins")]
+[assembly: System.Reflection.AssemblyProductAttribute("JenkinsMsTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("JenkinsMsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
